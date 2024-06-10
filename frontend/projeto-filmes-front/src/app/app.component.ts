f38848943dea17c1fb1d7807305bacbacbb7c264
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-06-10 14:23:23 -0300

    importando módulos

--- frontend/projeto-filmes-front/src/app/app.component.ts
+++ frontend/projeto-filmes-front/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {BuscaGeralComponent} from "./busca-geral/busca-geral.component";
+import {BuscaGeralComponent} from "./Components/busca-geral/busca-geral.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, BuscaGeralComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  template: `
+    <app-busca-geral/>
+  `
 })
 export class AppComponent {
-  title = 'projeto-filmes-front';
 }
