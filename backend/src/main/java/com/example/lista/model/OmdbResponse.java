483713348eb69862e968c3d505d8c85d344c46e8
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-06-10 09:56:17 -0300

    Criação do frontend

--- backend/src/main/java/com/example/lista/model/OmdbResponse.java
+++ backend/src/main/java/com/example/lista/model/OmdbResponse.java
@@ -1,9 +1,12 @@
 package com.example.lista.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 
+@Data
 public class OmdbResponse {
     @JsonProperty("Search")
     private List<Movie> search;
@@ -12,24 +15,12 @@ public class OmdbResponse {
     @JsonProperty("Response")
     private String response;
 
-    public List<Movie> getSearch() {
-        return search;
-    }
-
     public void setSearch(List<Movie> search) {
         this.search = search;
     }
 
-    public String getTotalResults() {
-        return totalResults;
-    }
-
     public void setTotalResults(String totalResults) {
         this.totalResults = totalResults;
-    }
-
-    public String getResponse() {
-        return response;
     }
 
     public void setResponse(String response) {
